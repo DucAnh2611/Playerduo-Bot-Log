9007b83383142ed65d79b635ea1db93e81847a02
ADMIN\Admin <ducanhmc24@gmail.com>
2024-12-12 19:03:39 +0700

    Add log after finish

--- db/models/rent.js
+++ db/models/rent.js
@@ -7,6 +7,8 @@ const RentSchema = new mongoose.Schema({
 
     duration: { type: Number, required: true },
     price: { type: Number, required: true },
+
+    sentNoti: { type: Boolean, required: false, default: false },
     status: { type: String, required: false, default: false },
     start: { type: Date, require: false, default: null },
     end: { type: Date, required: false, default: null },
@@ -16,6 +18,7 @@ RentSchema.pre("save", function () {
     const start = new Date();
     this.start = start;
     this.status = "RENTING";
+    this.sentNoti = false;
 
     this.end = convertTime(start, this.duration);
 });
